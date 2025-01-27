11f8b28affea53e30f0cac482ece5d746b72e432
Guhan-KM <guhan7766@gmail.com>
2025-01-27 17:54:21 +0530

    Create full_adder_tb.v

---
+++ Day 1/full_adder/full_adder_tb.v
@@ -0,0 +1,24 @@
+module fulladder_tb;
+reg a;
+reg b;
+reg cin;
+wire sum;
+wire cout;
+fulladder uut(.a(a),.b(b),.cin(cin),.sum(sum),.cout(cout));
+initial
+begin
+$dumpfile("fulladder_tb.vcd");
+$dumpvars;
+$monitor($time,"a=%b,b=%b,cin=%b,sum=%b,cout=%b",a,b,cin,sum,cout);
+a=0; b=0;cin=0;
+#5 a=0;b=0;cin=0;
+#5 a=0;b=0;cin=1;
+#5 a=0;b=1;cin=0;
+#5 a=0;b=1;cin=1;
+#5 a=1;b=0;cin=0;
+#5 a=1;b=0;cin=1;
+#5 a=1;b=1;cin=0;
+#5 a=1;b=1;cin=1;
+$finish();
+end
+endmodule
