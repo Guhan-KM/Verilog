dec53d11eb66a1c474c6893f1abbd6fb2611a324
Guhan-KM <guhan7766@gmail.com>
2025-02-06 10:11:49 +0530

    Create declaring wires.v

---
+++ HDL bits/basics/declaring wires.v
@@ -0,0 +1,14 @@
+module top_module(
+    input a,
+    input b,
+    input c,
+    input d,
+    output out,
+    output out_n   );
+    wire w0;
+    wire w1;
+    assign w0 = a&b;
+    assign w1 = c&d;
+    assign out = w0 | w1;
+    assign out_n = ~out;
+endmodule
